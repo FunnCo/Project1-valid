879dde0259b66abb85ec202695cb1e8c506fe065
FunnCo <41015524+FunnCo@users.noreply.github.com>
2018-08-12 20:36:32 +0300

    Tests for PersonController

--- src/test/java/PersonsControllerTest.java
+++ src/test/java/PersonsControllerTest.java
@@ -1,7 +1,11 @@
+import entities.Person;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class PersonsControllerTest {
@@ -10,7 +14,38 @@ public class PersonsControllerTest {
     @Test
     public void getPersonsTest() throws IOException, InterruptedException {
         PersonsController personsController = new PersonsController();
-        personsController.readPersonsFromFile("C:\\Users\\VaYls_Comp\\IdeaProjects\\Project1-valid\\example.csv");
+        personsController.readPersonsFromFile("D:\\Project1-valid\\example.csv");
         assertNotNull(personsController.getPersons());
+    }
+
+    @Test
+    public void getCountPersonTest() throws IOException, InterruptedException {
+        PersonsController personsController = new PersonsController();
+        personsController.readPersonsFromFile("D:\\Project1-valid\\example.csv");
+        assertEquals(3,(long)personsController.getCountPerson());
+    }
+
+    @Test
+    public void findPersonsByNameTest() throws IOException, InterruptedException {
+         PersonsController personsController = new PersonsController();
+         personsController.readPersonsFromFile("D:\\Project1-valid\\example.csv");
+         ArrayList<Person> arrayListOfPersons = personsController.findPersonsByName("Иванов Иван");
+
+         Person[] arrayOfPersonsActual = new Person[arrayListOfPersons.size()];
+         Person[] arrayOfPersonsExpected = new Person[1];
+         arrayListOfPersons.add(new Person("Иванов Иван|муж|19|163|+79623454527|Питер"));
+
+         assertArrayEquals(arrayOfPersonsExpected, arrayOfPersonsActual);
+    }
+
+    @Test
+    public void removeUserTest() throws IOException, InterruptedException {
+        PersonsController personsController = new PersonsController();
+        personsController.readPersonsFromFile("D:\\Project1-valid\\example.csv");
+
+        ArrayList<Person> personArrayList = personsController.getPersons();
+
+        personsController.removeUser(personArrayList.get(1));
+        assertEquals(2,(long)personsController.getCountPerson());
     }
 }
